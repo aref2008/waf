aaf6ea9093ac02f53dabccd26afb9d5dbcd7e482
aref2008 <aref.shaheed.sy@gmail.com>
2021-05-22 05:45:49 +0300

    modified:   waf_training.py

--- waf_training.py
+++ waf_training.py
@@ -30,46 +30,57 @@ def usage():
 	print ('*            															  ')
 	print ("*   Usage:")
 	print ("*   \t-d: dataset   (1 for HTTPParams, 2 for CSIC 3 for both)")
-	print ("*   \t-a: algorithm (1 for Naive Bayes, 2 for Logistic Regression, 3 for Decision Tree)")
+	print ("*   \t-a: algorithm (1 for Naive Bayes, 2 for Logistic Regression, 3 for Decision Tree, 4 for SVM)")
+	print ("*   \t-t: test percentage (value from 0.05 to 0.95 .. Default is 0.2)")
 	print ('*            															  ')
-	print ("*   example: waf_training.py -d 1 -a 2")
+	print ("*   example: waf_training.py -d 1 -a 2 -t 0.2")
 	print ('*            															  ')
 	print ('*            															  ')
 	print ('*******************************************************************************************\n')	
 
 
-def shuffls_dataset(option):
+
+def split_csv(filename):
+	global test_percentage
+	global train_percentage
+	print('[+] \t Test Percentage : '+str(test_percentage)+' \n')
+	print('[+] \t Train Percentage : '+str(train_percentage)+' \n')
+	dataset_file =  open(filename+'.csv', "r")
+	li = dataset_file.readlines()
+	header, rest =li[0], li[1:]
+	test_num = int(len(rest) * 0.20)
+	train_num = int(len(rest) * 0.80)
+	dataset_file.close()
+	random.shuffle(rest)
+	dataset_file2 = open(filename+"_shuffled.csv", "w")
+	new_lines = [header]+rest
+	dataset_file2.writelines(new_lines)
+	dataset_file2.close()
+	
+	dataset_file3 = open(filename+"_shuffled.csv", "r")
+	lines = dataset_file3.readlines()
+	test_records = lines[1:test_num]
+	train_records = lines[test_num+1:]
+	dataset_file3.close()
+
+	test_csv = open(filename+"_test.csv", "w")
+	new_test_lines = [header]+test_records
+	test_csv.writelines(new_test_lines)
+
+
+	train_csv = open(filename+"_train.csv", "w")
+	new_train_lines = [header]+train_records
+	train_csv.writelines(new_train_lines)
+
+
+def process_dataset(option):
 	option = int(option)
 	if(option == 1):
-		fid = open("HTTPParams.csv", "r")
-		li = fid.readlines()
-		header, rest=li[0], li[1:]
-		fid.close()
-		random.shuffle(rest)
-		fid = open("HTTPParams_shuffled.csv", "w")
-		new_lines = [header]+rest
-		fid.writelines(new_lines)
-		fid.close()
+		split_csv("HTTPParams")
 	elif(option == 2):
-		fid = open("CSIC.csv", "r")
-		li = fid.readlines()
-		header, rest =li[0], li[1:]
-		fid.close()
-		random.shuffle(rest)
-		fid = open("CSIC_shuffled.csv", "w")
-		new_lines = [header]+rest
-		fid.writelines(new_lines)
-		fid.close()	
+		split_csv("CSIC")
 	elif(option == 3):
-		fid = open("CSIC_HTTPParams.csv", "r")
-		li = fid.readlines()
-		header, rest =li[0], li[1:]
-		fid.close()
-		random.shuffle(rest)
-		fid = open("CSIC_HTTPParams_shuffled.csv", "w")
-		new_lines = [header]+rest
-		fid.writelines(new_lines)
-		fid.close()
+		split_csv('CSIC_HTTPParams')
 	else:
 		return False
 	
@@ -90,6 +101,10 @@ def choose_algorithm(option):
 		print('[+] \t Algorithm : Decision Tree  \n')
 		from sklearn.tree import DecisionTreeClassifier
 		algorithm = DecisionTreeClassifier()
+	elif(option == 4):
+		print('[+] \t Algorithm : SVM  \n')
+		from sklearn import svm
+		algorithm = svm.SVC(kernel='linear', cache_size=7000)
 	else:
 		return False
 		
@@ -98,70 +113,82 @@ def choose_algorithm(option):
 
 
 def choose_dataset(option):
-	global dataset
+	global test_dataset
+	global train_dataset
 	option = int(option)
 	if(option == 1):
 		print('[+] \t Dataset : HTTPParams 2015\n')
-		shuffls_dataset(option)
-		dataset_file = 'HTTPParams_shuffled.csv'
+		process_dataset(option)
+		dataset_file = 'HTTPParams'
 		col_names = ['payloads', 'payload_len','alpha','non_alpha','attack_feature','label']
 	elif(option == 2):
 		print('[+] \t Dataset : CSIC 2010\n')
-		shuffls_dataset(option)
-		dataset_file = 'CSIC_shuffled.csv'
+		process_dataset(option)
+		dataset_file = 'CSIC'
 		col_names = ['method', 'url', 'payloads', 'payload_len','alpha','non_alpha','attack_feature','label']
 	elif(option == 3):
 		print('[+] \t Dataset : CSIC 2010 & HTTPParams 2015\n')
-		shuffls_dataset(option)
-		dataset_file = 'CSIC_HTTPParams.csv'
+		process_dataset(option)
+		dataset_file = 'CSIC_HTTPParams'
 		col_names = ['payload_len','alpha','non_alpha','attack_feature','label']
 	else:
 		return False
 		
-	dataset = pd.read_csv(dataset_file, header=None, names=col_names, skiprows = 1)
-	# print(dataset)
-	# dataset = shuffle(dataset)
-	# print(dataset)
+	test_dataset = pd.read_csv(dataset_file+'_test.csv', header=None, names=col_names, skiprows = 1)
+	train_dataset = pd.read_csv(dataset_file+'_train.csv', header=None, names=col_names, skiprows = 1)
 
 	return True
 
 
 
-def train(ds, al):
+def train(ds, al, test, train):
 	check_if_ds_selected = choose_dataset(ds)
 	check_if_al_selected = choose_algorithm(al)
+	check_test_percentage = False
+	check_train_percentage = False
 
+	if(float(test) < 0.05 or float(test) > 0.95):
+		print('check your input by reading usage below !!')
+		usage()
+		exit()
+		
+	if(float(train) < 0.05 or float(train) > 0.95):
+		print('check your input by reading usage below !!')
+		usage()
+		exit()
+		
 	if(not check_if_ds_selected or not check_if_al_selected):
 		print('check your input by reading usage below !!')
 		usage()
 		exit()
 
 	global algorithm
-	global dataset
+	global test_dataset
+	global train_dataset
 	feature_cols = ['payload_len','alpha','non_alpha','attack_feature']
 
-	X = dataset[feature_cols]
-	y = dataset.label
-
-	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-	algorithm.fit(X_train, y_train)
-	y_pred_class = algorithm.predict(X_test)
-	algorithm.fit(X_train, y_train)
-
-
-
-	print('[+] \t Classification accuracy : ', "{:.2f}".format(metrics.accuracy_score(y_test, y_pred_class) * 100), '%\n')
+	X1 = test_dataset[feature_cols]
+	Y1 = test_dataset.label
+	X2 = train_dataset[feature_cols]
+	Y2 = train_dataset.label
+	algorithm.fit(X2, Y2)
+	Y2_pred = algorithm.predict(X1)
+	algorithm.fit(X2, Y2)
 
 
-	print('[+] \t Percentage of Anomaly requests in test set : ', "{:.2f}".format(y_test.mean()*100), '%\n' )
-	print('[+] \t Percentage of Normal requests in test set : ', "{:.2f}".format((1 - y_test.mean()) * 100), '%\n' )
+
+	print('[+] \t Classification accuracy : ', "{:.2f}".format(metrics.accuracy_score(Y1, Y2_pred) * 100), '%\n')
 
 
+	print('[+] \t Percentage of Anomaly requests in test set : ', "{:.2f}".format(Y1.mean()*100), '%\n' )
+	print('[+] \t Percentage of Normal requests in test set : ', "{:.2f}".format((1 - Y1.mean()) * 100), '%\n' )
 
 
 
 
-	confusion = metrics.confusion_matrix(y_test, y_pred_class)
+
+
+	confusion = metrics.confusion_matrix(Y1, Y2_pred)
 	TP = confusion[1, 1]
 	TN = confusion[0, 0]
 	FP = confusion[0, 1]
@@ -174,11 +201,11 @@ def train(ds, al):
 
 
 	print('\n[+] \t Metrics : ')
-	print('\t[-]  Accuracy Score (train_test_split): ', "{:.2f}".format(metrics.accuracy_score(y_test, y_pred_class) * 100), '%')
-	print('\t[-]  Accuracy Score (k-fold): ', "{:.2f}".format(cross_val_score(algorithm, X, y, cv=100, scoring='accuracy').mean() * 100), '%')
+	print('\t[-]  Accuracy Score (train_test_split): ', "{:.2f}".format(metrics.accuracy_score(Y1, Y2_pred) * 100), '%')
+	print('\t[-]  Accuracy Score (k-fold): ', "{:.2f}".format(cross_val_score(algorithm, X2, Y2, cv=100, scoring='accuracy').mean() * 100), '%')
 	
-	print('\t[-]  Classification Error : ', "{:.2f}".format((1- metrics.accuracy_score(y_test, y_pred_class)) * 100), '%')
-	print('\t[-]  Sensitivity : ', "{:.2f}".format(metrics.recall_score(y_test, y_pred_class) * 100), '%')
+	print('\t[-]  Classification Error : ', "{:.2f}".format((1- metrics.accuracy_score(Y1, Y2_pred)) * 100), '%')
+	print('\t[-]  Sensitivity : ', "{:.2f}".format(metrics.recall_score(Y1, Y2_pred) * 100), '%')
 	specificity = TN / (TN + FP)
 	print('\t[-]  Specificity : ', "{:.2f}".format(specificity * 100), '%')
 	false_positive_rate = FP / float(TN + FP)
@@ -193,20 +220,29 @@ def start(argv):
 		usage()
 		sys.exit()
 	try:
-		opts, args = getopt.getopt(argv, "a:d:")
-	except getopt.GetoptError:
+		opts = {}		
+		for i in range(0, len(argv), 2):
+			opts[argv[i]] =  argv[i+1]
+
+	except :
 		print ("Error in arguments")
 		usage()
 		sys.exit()
-
-	for opt, arg in opts:
+	global test_percentage
+	global train_percentage
+	test_percentage = 0.2
+	train_percentage = 0.8
+	for opt in opts:
 		if opt == '-a':
-			algorithm = arg
+			algorithm = opts[opt]
 		elif opt == '-d':
-			dataset = arg
+			dataset = opts[opt]
+		elif opt == '-t':
+			test_percentage = float(opts[opt])
+			train_percentage = 1- test_percentage
 
 	try:
-		train(dataset, algorithm)
+		train(dataset, algorithm, test_percentage, train_percentage)
 	except Exception as e:
 		print('Exception !!')
 		print(e)
